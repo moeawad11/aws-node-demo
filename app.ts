e201cb8a99b9a1e6cecd7600c30fef0b7ee6809b
moeawad11 <moeawad2004@hotmail.com>
2025-11-15 21:21:47 +0200

    Add Multer middleware and S3 file upload endpoint

--- app.ts
+++ app.ts
@@ -2,14 +2,22 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { pool } from "./db.js";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import multer from "multer";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-const s3 = new S3Client({ region: "eu-north-1" });
+const s3 = new S3Client({
+  region: "eu-north-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
 const BUCKET = process.env.BUCKET;
+const upload = multer();
 
 app.get("/health", (req: Request, res: Response) => {
   res.json({ ok: true });
@@ -83,6 +91,32 @@ app.post("/upload", async (req: Request, res: Response) => {
     res.status(500).json({ message: "Error uploading to S3" });
   }
 });
+
+app.post(
+  "/upload-file",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    try {
+      if (!req.file) res.status(400).json({ message: "File required." });
+
+      const key = `${Date.now()}-${req.file?.originalname}`;
+
+      await s3.send(
+        new PutObjectCommand({
+          Bucket: BUCKET,
+          Key: key,
+          Body: req.file?.buffer,
+          ContentType: req.file?.mimetype,
+        })
+      );
+
+      res.status(201).json({ message: "Uploaded to file", key });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "S3 upload failed" });
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
